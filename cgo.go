aa4499a473ad10b36e862d2724b47771c3dea44c
kumahooks <meryl@inventati.org>
2026-08-30 12:54:18 -0300

    comments open/close functions

--- cgo.go
+++ cgo.go
@@ -17,14 +17,24 @@ var (
 	ErrInvalid = errors.New("invalid audio file")
 )
 
+// AudioFile represents the taglib's TagLib_File, TagLib_Tag, and TagLib_AudioProperties of a given audio file.
 type AudioFile struct {
 	handle          *C.TagLib_File
 	tag             *C.TagLib_Tag
 	audioProperties *C.TagLib_AudioProperties
 }
 
-func Open(filepath string) (*AudioFile, error) {
-	cString := C.CString(filepath)
+// Open receives a file path, and either return AudioFile, or an error.
+//
+// there are two ways we can fail here:
+// - taglib_file_new returns null: in this case, taglib could not determine or open the file;
+// - taglib_file_is_valid returns false: in this case, taglib either deems the file not opened, unreadable, or could not
+// find valid informations for TagLib_Tag or TagLib_AudioProperties.
+//
+// in case taglib_file_is_valid is false, the byte stream has already been created by taglib with taglib_file_new, so we
+// free it using taglib_file_free(file) before we return ErrInvalid.
+func Open(filePath string) (*AudioFile, error) {
+	cString := C.CString(filePath)
 	defer C.free(unsafe.Pointer(cString))
 
 	fileHandle := C.taglib_file_new(cString)
@@ -44,6 +54,7 @@ func Open(filepath string) (*AudioFile, error) {
 	}, nil
 }
 
+// Close frees the memory allocated to the audio file's bytestream with taglib's taglib_file_free function.
 func (file *AudioFile) Close() {
 	if file == nil || file.handle == nil {
 		return
